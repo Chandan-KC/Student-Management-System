308a77fc7376a42b8eca1e3339c7beaa19272aeb
Chandan-KC <1js19cv408@gmail.com>
2025-12-08 17:49:30 +0530

    Imported ConnectDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,46 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const mongoose =require("mongoose");
+// // import mongoose from "mongoose";
+// // const ConnectDB = require("./connection/db");
+
+// // app.use(express(),json);
+// // app.use(cors());
+
+// const PORT = 3000;
+
+// async function ConnectDB(){
+//     try{
+//         await mongoose.connect("mongodb+srv://1js19cv408_db_user:oEbNg2xOLzsYRgvs@studentmanagement.paskwop.mongodb.net/?appName=StudentManagement");
+//         console.log("Connected MongoDB")
+//     }
+//     catch(error){
+//         console.log("Error connecting to MongoDB ",error);
+//     }
+// }
+
+// ConnectDB();
+
+// app.listen(PORT, ()=>{
+//     console.log("Your Application is running on PORT ",PORT);
+// })
+
+// #########################################
+
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const ConnectDB = require("./connection/db");
+app.use(express.json());
+app.use(cors());
+
 const PORT = 3000;
 
-app.listen(PORT, ()=>{
-    console.log("Your Application is running on PORT ",PORT);
-})
+ConnectDB();
+
+app.listen(PORT, () => {
+  console.log("Application is running in PORT", PORT);
+});
+
+
